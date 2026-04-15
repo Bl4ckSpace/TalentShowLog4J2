09558ce32a8ffc39e29242f0055c999bb1bf6ad9
FJstu <frodpla916@g.educaand.es>
2026-04-15 09:10:00 +0200

    TalentShowLog4JEjercicio

---
+++ src/main/java/prtalentshowamp/beansProcesos/Principal.java
@@ -0,0 +1,10 @@
+package prtalentshowamp.beansProcesos;
+
+
+public class Principal {
+    public static void main(String[] args) {
+        Concurso concurso;
+        concurso = new Concurso();
+        concurso.ejecutar();
+   }
+}
